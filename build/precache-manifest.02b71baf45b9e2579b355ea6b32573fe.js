9d1844e29cc79b1003127d5c0c4f1b2e05d36466
michaelhaneman <mhaneman@calpoly.edu>
2021-04-16 12:21:21 -0700

    first commit

---
+++ build/precache-manifest.02b71baf45b9e2579b355ea6b32573fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b4e696a637a4a6b4f79b6d18968b54",
+    "url": "/searchAPI-recipe/index.html"
+  },
+  {
+    "revision": "99258005c11a63c15a91",
+    "url": "/searchAPI-recipe/static/css/main.bca79ee0.chunk.css"
+  },
+  {
+    "revision": "858e94b6d0513781cceb",
+    "url": "/searchAPI-recipe/static/js/2.dee1f4f9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/searchAPI-recipe/static/js/2.dee1f4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99258005c11a63c15a91",
+    "url": "/searchAPI-recipe/static/js/main.89740f84.chunk.js"
+  },
+  {
+    "revision": "dadb073763d9188ca8c2",
+    "url": "/searchAPI-recipe/static/js/runtime-main.416e9b8d.js"
+  }
+]);
